9038fa807b32c77cb46a308ee06d05826e7859ec
Konda Yoshifumi <kndys.fm+github@gmail.com>
2023-01-06 16:51:33 +0900

    Read HID input report from Sony DualShock4

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
 extern crate hidapi;
 
-use hidapi::HidApi;
+use hidapi::{HidApi, HidDevice};
+
+/// from "https://bitbucket.org/unessa/dualshock4-rust/src/master/src/dualshock4/mod.rs"
+const DUALSHOCK4_VENDOR_ID:u16 = 0x54c;
+// Dualshock4 product ID changed after playstation update 5.50
+const DUALSHOCK4_PRODUCT_ID_NEW:u16 = 0x9cc;
+const DUALSHOCK4_PRODUCT_ID_OLD:u16 = 0x5c4;
+
+fn start(hid: HidDevice) {
+    let mut buf = [0u8; 78];
+    let timeout =  1000;
+    
+    loop {
+        match hid.read_timeout(&mut buf, timeout) {
+            Ok(sz) => {
+                println!("Read ({} bytes): {:?}", sz, &buf[..sz]);
+            },
+            Err(_) => {
+
+            }
+        }
+    }
+}
 
 fn main() {
     match HidApi::new() {
         Ok(api) => {
-            let mut devs: Vec<_> = api.device_list().collect();
-            devs.sort_by_key(|d| d.product_id());
-            devs.sort_by_key(|d| d.vendor_id());
-            for device in devs {
-                println!("PID:{:04X}_VID:{:04X}&UP:{:04X}_U:{:04X}", 
-                        device.vendor_id(), device.product_id(),
-                        device.usage_page(), device.usage());
-                if let Ok(hid) = device.open_device(&api) {
-                    if let Ok(man) = hid.get_manufacturer_string() {
-                        println!("  manufacturer: {}", man.unwrap());
+            for info in api.device_list() {
+                if info.vendor_id() == DUALSHOCK4_VENDOR_ID && (
+                    info.product_id() == DUALSHOCK4_PRODUCT_ID_NEW ||
+                    info.product_id() == DUALSHOCK4_PRODUCT_ID_OLD ) {
+                    println!("DualShock4 was found");
+                    if let Ok(hid) = info.open_device(&api) {
+                        println!("Succeeded to open DualShock4");
+                        start(hid);
+                        break;
                     } else {
-                        println!("  failed to get manufacturer");
-                    }
-                    if let Ok(prd) = hid.get_product_string() {
-                        println!("  product name: {}", prd.unwrap());
-                    } else {
-                        println!("  failed to get product name");
-                    }
-                    // try `let...else...` statement
-                    let Ok(sn) = hid.get_serial_number_string() else {
-                        println!("  failed to get serial number");
+                        println!("Failed to open DualShock4");
                         continue;
-                    };
-                    println!("  serial number: {}", sn.unwrap());
-                } else {
-                    println!("  it cannot be opened");
-                    continue;
+                    }
                 }
             }
         },
@@ -39,4 +47,5 @@ fn main() {
             eprintln!("Error: {}", e);
         },
     }
+    println!("DualShock4 was not found");
 }
